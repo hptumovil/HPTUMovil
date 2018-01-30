1e4dda0135648a2796587b82c4265cde2eb4eed3
unknown <1039285274@PCHPTU02519.hptu.org.co>
2018-01-30 15:45:01 -0500

    Se agregor la funcion como llegar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { Settings } from '../providers/providers';
 import { User } from '../providers/providers';
 import { Api } from '../providers/providers';
 import { MyApp } from './app.component';
+import { Network } from '@ionic-native/network';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 // The translate loader needs to know where to load i18n files
 // in Ionic's static asset pipeline.
@@ -52,6 +55,7 @@ export function provideSettings(storage: Storage) {
     }),
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -66,7 +70,9 @@ export function provideSettings(storage: Storage) {
     StatusBar,
     { provide: Settings, useFactory: provideSettings, deps: [Storage] },
     // Keep this to enable Ionic's runtime error handling during development
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Network,
+    Geolocation
   ]
 })
 export class AppModule { }
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Network } from '@ionic-native/network';
 
 /**
  * Generated class for the MapPage page.
@@ -8,6 +10,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+declare var google;
+
 @IonicPage()
 @Component({
   selector: 'page-map',
@@ -15,11 +19,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('map') mapElement: ElementRef;
+  @ViewChild('directionsPanel') directionsPanel: ElementRef;
+
+  //The GoogleMaps data for api conection goes here
+  map: any;
+  mapInitialised: boolean = false;
+  origin = new google.maps.LatLng();
+  apiKey = 'AIzaSyAuYJr53z_ljCFv7iVbY5YX39HJOXMfIUo';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private network: Network, private geolocation: Geolocation) {    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
+    this.showMap();
+    this.startNavigating();
   }
 
+  showMap(){
+    //location Lat & Long
+    const location = new google.maps.LatLng(6.2770054, -75.5807295);    
+    
+    //Map options
+     const options = {
+      center: location,
+      zoom: 15,
+      streetViewControl: false,
+      mapTypeId: 'roadmap'
+    }
+    
+    //Create new Google Maps with center in Hospital Pablo Tobon Uribe
+    this.map = new google.maps.Map(this.mapElement.nativeElement, options);    
+  }
+    
+
+  startNavigating(){
+
+    //Get the current location from the device
+    this.geolocation.getCurrentPosition().then((position) => {
+ 
+      this.origin = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    }, (err) => {
+        console.log(err);
+      });
+ 
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer;
+
+    directionsDisplay.setMap(this.map);
+    directionsDisplay.setPanel(this.directionsPanel.nativeElement);
+
+    directionsService.route({
+        origin: {lat: 6.2799226, lng: -75.5718324},
+        destination: {lat: 6.2770054, lng: -75.5807295},
+        travelMode: google.maps.TravelMode['DRIVING']
+    }, (res, status) => {
+
+        if(status == google.maps.DirectionsStatus.OK){
+            directionsDisplay.setDirections(res);
+        } else {
+            console.warn(status);
+        }
+
+    });
+
+}
 }
