8b19acb2686df29b80d3e23f0fc3002788511db0
Juan David Torres <1039285274@hptu.org.co>
2018-02-14 09:38:12 -0500

    Se gregaron las validaciones de los campos del formulario contactenos

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the ContactPage page.
@@ -14,12 +15,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'contact.html',
 })
 export class ContactPage {
+  slideOneForm: FormGroup;
+  submitAttempt: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.slideOneForm = formBuilder.group({
+      name: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      phone: [''],
+      message: ['', Validators.compose([Validators.required])]
+  });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
   }
+
+  save(){
+    this.verify();
+ 
+    
+}
+
+verify(){
+  this.submitAttempt = true;
+ 
+    if(!this.slideOneForm.valid){
+      console.log("fail!")
+    }
+    else {
+        console.log("success!")
+        console.log(this.slideOneForm.value);        
+    } 
+}
 
 }
